02520e4094b3bd01f05c42f9bf476a746b0429a6
Ghadaj <ghada.aljurbua@gmail.com>
2022-06-15 16:17:02 +0300

    Initial Commit

---
+++ etmam/etmamApp.swift
@@ -0,0 +1,17 @@
+//
+//  etmamApp.swift
+//  etmam
+//
+//  Created by Ghada on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct etmamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
